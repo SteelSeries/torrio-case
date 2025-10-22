1a2298b01e42002955c49a0a088ee5f1c0912f8d
TimmyTien <149564747+TimmyTien@users.noreply.github.com>
2025-10-22 12:08:00 +0800

    Revert "Merge commit '2a6b91404ca03aeb8026bf0644d1d2ede63db210' into stage-working-sample"
    This reverts commit cb5593f59c9943d23ab0767e2471baad552b1f96, reversing
    changes made to 7adda6ed3005e1990aab1f7b40e927bed1975132.

--- App/project/at_start_f415/Torrio_Case_App/inc/Commands.h
+++ App/project/at_start_f415/Torrio_Case_App/inc/Commands.h
@@ -4,51 +4,49 @@
  *************************************************************************************************/
 #include "at32f415_board.h"
 
+
 /*************************************************************************************************
  *                                   GLOBAL MACRO DEFINITIONS                                    *
  *************************************************************************************************/
 // firmware info
-#define VERSION_OP 0x10U // version
+#define VERSION_OP                  0x10U // version
 
 // MCU control
-#define RESET_OP 0x01U // recovery module
+#define RESET_OP                    0x01U // recovery module
 
 // For firmware/file update
-#define ERASE_FILE_OP 0x02U
-#define FILE_ACCESS_OP 0x03U
-#define FILE_CRC32_OP 0x04U
+#define ERASE_FILE_OP               0x02U 
+#define FILE_ACCESS_OP              0x03U
+#define FILE_CRC32_OP               0x04U
 
 // debug
-#define DEBUG_CUSTOM_OP 0x08U // each application implements it as they see fit
-#define DEBUG_SY8809_OP 0x71U
-#define DEBUG_SY8809_XSENSE_OP 0x70U
-
-// Case
-#define GET_BATTERY_INFO 0x37 // Read Case and buds battery level and charging status.
+#define DEBUG_CUSTOM_OP             0x08U // each application implements it as they see fit
+#define DEBUG_SY8809_OP             0x71U 
+#define DEBUG_SY8809_XSENSE_OP      0x70U 
 
 // internal - we can reuse those as USB APIs for Artery
-#define INTERNAL_USB_SUSPEND_OP 0x0CU // USB entered suspend mode. Lower power usage
-#define INTERNAL_USB_RESUME_OP 0x0DU  // after suspension, resume from device received
+#define INTERNAL_USB_SUSPEND_OP     0x0CU // USB entered suspend mode. Lower power usage
+#define INTERNAL_USB_RESUME_OP      0x0DU // after suspension, resume from device received
 
 // factory
-#define FAC_GET_BATTERY_AND_NTC 0x72 // factory settings
-#define FAC_SET_CHARGE_STATUS 0x74   // factory settings
+#define FAC_GET_BATTERY_AND_NTC     0x72 // factory settings
+#define FAC_SET_CHARGE_STATUS       0x74 // factory settings
 
 // factory settings
-#define FAC_SERIAL_OP 0x13           // factory settings
-#define FAC_MODEL_COLOR_SPIN_OP 0x6D // factory settings
+#define FAC_SERIAL_OP               0x13 // factory settings
+#define FAC_MODEL_COLOR_SPIN_OP     0x6D // factory settings
 
-#define NO_OP 0x00U // command module
+#define NO_OP                       0x00U // command module
 
-#define NORMAL_MODE 0x01U
-#define BOOTLOADER_MODE 0x10U
+#define NORMAL_MODE                 0x01U
+#define BOOTLOADER_MODE             0x10U
 
-#define COMMAND_READ_FLAG 0x80U
+#define COMMAND_READ_FLAG           0x80U
 
-#define FLASH_WRITE_ERRORS 0x03U
-#define FLASH_OPERATION_SUCCESS 0x00U
+#define FLASH_WRITE_ERRORS          0x03U
+#define FLASH_OPERATION_SUCCESS     0x00U
 
-#define USB_RECEIVE_LEN 1024U
+#define USB_RECEIVE_LEN             1024U
 /*************************************************************************************************
  *                                    GLOBAL TYPE DEFINITIONS                                    *
  *************************************************************************************************/
@@ -67,18 +65,12 @@ typedef enum
     COMMAND_TARGET_RIGHT_BUD
 } Command_Target_t;
 
-typedef enum
-{
-    COMMAND_GET_BATTERY_STATUS_UNPLUGGED = 0x00,
-    COMMAND_GET_BATTERY_STATUS_CHARGE,
-    COMMAND_GET_BATTERY_STATUS_COMPLETE
-} Command_GetBatteryStatus_t;
-
 /*************************************************************************************************
  *                                  GLOBAL VARIABLE DECLARATIONS                                 *
  *************************************************************************************************/
 
+
 /*************************************************************************************************
  *                                  GLOBAL FUNCTION DECLARATIONS                                 *
  *************************************************************************************************/
-void Commands_HandleUsbCommand(const uint8_t *in, size_t in_len);
+void Commands_HandleUsbCommand(const uint8_t * in, size_t in_len);
