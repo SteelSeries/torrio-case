57325bf13e0dab72123678c5e265a363c0fda296
evachou-0220 <eva.chou@steelseries.gg>
2025-11-10 17:54:08 +0800

    Subject: TMR3 init
    1.lighting.c initial move to TMR3

--- App/project/at_start_f415/Torrio_Case_App/src/lighting.c
+++ App/project/at_start_f415/Torrio_Case_App/src/lighting.c
@@ -195,12 +195,6 @@ void Lighting_GpioConfigHardware(const Lighting_HardwareSettings_t *hardware_set
 
     gpio_default_para_init(&gpio_init_struct);
 
-    crm_clocks_freq_type crm_clocks_freq_struct = {0};
-
-    crm_clocks_freq_get(&crm_clocks_freq_struct);
-
-    crm_periph_clock_enable(CRM_TMR3_PERIPH_CLOCK, TRUE);
-
     gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
     gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
     gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
